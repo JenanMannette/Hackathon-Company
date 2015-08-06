871dd3d22e4f5f24da5bbd604f54c914f736ded4
Jenan Mannette <jenanmannette@gmail.com>
2015-08-06 11:41:14 -0600

    login/logout

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,17 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/login', function (req, res, next) {
+  res.render('session/login', {title:'Login'});
+});
+
+router.get('/logout', function (req, res, next) {
+  req.session=null;
+  res.redirect('/')
+});
+
+router.get('/register', function (req, res, next) {
+    res.render('session/register', {title:'Register'});
+})
+
 module.exports = router;
