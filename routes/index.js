7846c4facf94cb8e10c3ef8f67e88da3580f9fe5
John Alfano <alf027@gmail.com>
2015-08-06 13:04:17 -0600

    added some logic to use glassdoor api

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,25 @@
 var express = require('express');
 var router = express.Router();
+var axios = require('axios');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   console.log(req.user)
   res.render('index', { title: 'Express' });
+});
+
+router.get('/companySearch',function(req,res) {
+
+  console.log(req.query.companyName);
+  axios.get('http://api.glassdoor.com/api/api.htm?t.p=40412&t.k=fvkSeCJKgSQ&userip=0.0.0.0&useragent=&format=json&v=1&l=boulder&q='+ req.query.companyName +'&action=employers')
+    .then(function (response) {
+      console.log(response);
+      res.render('companySearch', response.employers)
+    })
+    .catch(function (response) {
+      console.log(response);
+    });
+
 });
 
 router.get('/login', function (req, res, next) {
